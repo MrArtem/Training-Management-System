256fbc20f9cc310825d850ec8601671953a9f092
AlexZapolski <lelik_lesha@hotmail.com>
2015-10-05 09:54:00 +0300

    db-tms-5

--- training/src/main/java/com/exadel/training/dao/domain/Lesson.java
+++ training/src/main/java/com/exadel/training/dao/domain/Lesson.java
@@ -1,7 +1,54 @@
 package com.exadel.training.dao.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * Created by ayudovin on 05.10.2015.
  */
+@Entity
+@Table
 public class Lesson {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private Date date;
+
+    private String place;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Training training;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public Training getTraining() {
+        return training;
+    }
+
+    public void setTraining(Training training) {
+        this.training = training;
+    }
 }
