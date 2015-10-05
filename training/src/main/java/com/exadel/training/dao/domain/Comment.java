cc71616c23fadd22b0ca726971192a53e1196e0c
Darya Rovdo <d.rovdo@gmail.com>
2015-10-05 09:50:06 +0300

    Create Tag and Comment db entities

--- training/src/main/java/com/exadel/training/dao/domain/Comment.java
+++ training/src/main/java/com/exadel/training/dao/domain/Comment.java
@@ -1,7 +1,101 @@
 package com.exadel.training.dao.domain;
 
-/**
- * Created by ayudovin on 05.10.2015.
- */
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Comment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Training training;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private User user;
+
+    private boolean clear;
+
+    private boolean interesting;
+
+    private boolean newMaterial;
+
+    private int effective;
+
+    private boolean recommendation;
+
+    private String other;
+
+    public Comment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Training getTraining() {
+        return training;
+    }
+
+    public void setTraining(Training training) {
+        this.training = training;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public boolean isClear() {
+        return clear;
+    }
+
+    public void setClear(boolean clear) {
+        this.clear = clear;
+    }
+
+    public boolean isInteresting() {
+        return interesting;
+    }
+
+    public void setInteresting(boolean interesting) {
+        this.interesting = interesting;
+    }
+
+    public boolean isNewMaterial() {
+        return newMaterial;
+    }
+
+    public void setNewMaterial(boolean newMaterial) {
+        this.newMaterial = newMaterial;
+    }
+
+    public int getEffective() {
+        return effective;
+    }
+
+    public void setEffective(int effective) {
+        this.effective = effective;
+    }
+
+    public boolean isRecommendation() {
+        return recommendation;
+    }
+
+    public void setRecommendation(boolean recommendation) {
+        this.recommendation = recommendation;
+    }
+
+    public String getOther() {
+        return other;
+    }
+
+    public void setOther(String other) {
+        this.other = other;
+    }
 }
--- training/src/main/java/com/exadel/training/dao/domain/Tag.java
+++ training/src/main/java/com/exadel/training/dao/domain/Tag.java
@@ -1,7 +1,51 @@
 package com.exadel.training.dao.domain;
 
-/**
- * Created by ayudovin on 05.10.2015.
- */
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table
 public class Tag {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToMany(mappedBy = "tagList")
+    private List<Training> training;
+
+    private String specialty;
+
+    private String colour;
+
+    public Tag() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<Training> getTraining() {
+        return training;
+    }
+
+    public void setTraining(List<Training> training) {
+        this.training = training;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 }
