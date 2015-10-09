4004a861371edd7df1e6c7a7d279687d9ed2cf8a
Anton Dosov <dosantinc@gmail.com>
2015-10-09 07:48:55 +0300

    local auth

---
+++ TRE-FrontEnd/src/app/services/auth.service.js
@@ -0,0 +1,119 @@
+(function () {
+    'use strict';
+    angular
+        .module('tmsAPI')
+        .factory('authService', authService);
+
+    /* @ngInject */
+    function authService($cookies, $state) {
+        var accessRights = {
+            admin: 0,
+            user: 1,
+            exCoach: 2,
+            exUser: 3,
+            noRights: 4,
+            accessRightsFromString: accessRightsFromString
+        };
+
+
+        var isLogged = false;
+        var user = {
+            login: "",
+            username: "",
+            accessRights: 4,
+        };
+
+
+        var authService = {
+            login: login,
+            credsLogin: credsLogin,
+            logout: logout,
+            isLogged: getIsLogged,
+            getUser: getUser,
+            getAccessRights: getAccessRights
+        };
+        
+        return authService;
+
+        //functions
+        
+        function getIsLogged() {
+            return isLogged
+        }
+
+        function login(username, password, isRemember) {
+            //Perform Server log in
+
+            // success :
+            isLogged = true;
+            user.login = username;
+            user.password = password;
+            user.accessRights = accessRights.accessRightsFromString("ADMIN");
+            
+            if(isRemember){
+                putUserCreds(user);
+            }
+            
+            return true;
+            //return success promise
+
+            //error:
+            //return error promise
+
+
+        }
+        
+        function credsLogin(){
+            var userCreds = getUserCreds()
+            if(userCreds){
+                user = userCreds;
+                isLogged = true;
+            }  
+        }
+
+        function logout() {
+            // server logout
+            // just logout
+            isLogged = false;
+            user = {};
+            clearUserCreds();
+            $state.go('login');
+        }
+
+        function getUser() {
+            return angular.copy(user);
+        }
+
+        function getAccessRights() {
+            return user.accessRights;
+        }
+
+        function accessRightsFromString(accessString) {
+            
+            if (accessString === "ADMIN")
+                return this.admin;
+            if (accessString === "USER")
+                return this.user;
+            if (accessString === "EX_COACH")
+                return this.exCoach;
+            if (accessString === "EX_USER")
+                return this.exUser;
+            
+            return this.noRights;    
+        }
+        
+        function putUserCreds(user) {
+            $cookies.putObject('user', user);
+        }
+        function getUserCreds(){
+            return $cookies.getObject('user');
+        }
+        function clearUserCreds(){
+            return $cookies.remove('user');
+        }
+
+
+
+    }
+
+})();
