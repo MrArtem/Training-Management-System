e58ed839790de0e3d5fc0760fa37d77369dab934
unknown <aaneychik@aaneychik0210.exadel.by>
2015-10-12 11:52:31 +0300

    create course form wrong time problem

--- TRE-FrontEnd/src/app/page.createcourse/createcourse.step4/step4.controller.js
+++ TRE-FrontEnd/src/app/page.createcourse/createcourse.step4/step4.controller.js
@@ -6,9 +6,33 @@
         .controller('Step4Controller', Step4Controller);
 
     /** @ngInject */
-    function Step4Controller() {
+    function Step4Controller($scope, $state) {
         var vm = this;
+        vm.manualDates = [];
+        vm.dateTime = {};
+        vm.repeat = {};
+        vm.addDate = addDate;
+        vm.changeTab = changeTab;
 
+        vm.days = ['M', 'T', 'W', 'Th', 'F', 'Sa', 'S'];
+        vm.onDays = [false, false, false, false, false, false, false];
+        vm.dates = [];
 
+        for(var i = 0; i < 7; i++) {
+            $scope.tempDates.push({hours: 0, minutes: 0, place: ""});
+        }
+
+        function addDate() {
+            $scope.courseInfo.lessonList.push((new Date(vm.dateTime.date + " " + vm.dateTime.hours + ":" + vm.dateTime.minutes)).getTime());
+        }
+
+        function changeTab() {
+            if($scope.courseInfo.isRepeating) {
+                $state.transitionTo('createcourse.step4.repeat');
+            }
+            else {
+                $state.transitionTo('createcourse.step4.manual');
+            }
+        }
     }
 })();
