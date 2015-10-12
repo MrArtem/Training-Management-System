9b98523a48216bb2961d2f6bd582bb2de5456a30
Darya Rovdo <d.rovdo@gmail.com>
2015-10-12 10:03:25 +0300

    Fix configuration

--- training/src/main/java/com/exadel/training/Main.java
+++ training/src/main/java/com/exadel/training/Main.java
@@ -1,19 +1,13 @@
 package com.exadel.training;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 
 /**
  * Created by ayudovin on 01.10.2015.
  */
 
-@EnableAutoConfiguration
-@EnableWebMvc
+@ComponentScan("com.exadel.training.configuration")
 public class Main {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
