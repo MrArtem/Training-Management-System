97bcdc2306769b39874bed0de7ca22f350087f05
Darya Rovdo <d.rovdo@gmail.com>
2015-10-15 20:01:24 +0300

    Add getTrainingList

---
+++ training/src/main/java/com/exadel/training/controller/model/TrainingListModel.java
@@ -0,0 +1,7 @@
+package com.exadel.training.controller.model;
+
+/**
+ * Created by drovdo on 10/14/2015.
+ */
+public class TrainingListModel {
+}
--- training/src/main/java/com/exadel/training/dao/domain/test.java
+++
@@ -1,7 +0,0 @@
-package com.exadel.training.dao.domain;
-
-/**
- * Created by azapolski on 10/2/2015.
- */
-public class test {
-}
