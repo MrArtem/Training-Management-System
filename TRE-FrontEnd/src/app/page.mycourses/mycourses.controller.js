a7ca39fb3cb7debbaf05020f91d2ef3948b91c93
unknown <aaneychik@aaneychik0210.exadel.by>
2015-10-21 09:15:55 +0300

    some style changes

--- TRE-FrontEnd/src/app/page.mycourses/mycourses.controller.js
+++ TRE-FrontEnd/src/app/page.mycourses/mycourses.controller.js
@@ -19,7 +19,7 @@
             nextLesson: 2,
             nextDate: '15.10.2015',
             nextPlace: 'unit 243',
-            shortInfo: 'I’ve worked with Angular on quite a few apps at this point, and have seen many different ways to structure them. I’m writing a book on architecting Angular apps right now with the MEAN stack and as such have researched heavily into this specific topic.',
+            shortInfo: 'I\'ve worked with Angular on quite a few apps at this point, and have seen many different ways to structure them. I\'m writing a book on architecting Angular apps right now with the MEAN stack and as such have researched heavily into this specific topic.',
             coachName: 'Papa John',
             coachId: 1,
             isCoach: false
