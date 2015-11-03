0f14ce0c1b7dda75536b35a9d2c513c0f739db1e
unknown <aaneychik@aaneychik0210.exadel.by>
2015-11-03 12:35:56 +0300

    websockets

--- TRE-FrontEnd/src/app/page.admin/admin.approval/admin.approval.controller.js
+++ TRE-FrontEnd/src/app/page.admin/admin.approval/admin.approval.controller.js
@@ -6,20 +6,76 @@
         .controller('ApprovalController', ApprovalController);
 
     /** @ngInject */
-    function ApprovalController(adminAPI) {
+    function ApprovalController($scope, $stomp, adminAPI) {
         var vm = this;
+        var stompClient = null;
 
         vm.approveList = [];
-        vm.getApproveList = getApproveList;
+        vm.disconnect = disconnect;
         vm.makeText = makeText;
+        vm.send = send;
+        vm.connect = connect;
 
-        vm.getApproveList();
+        vm.connect();
 
-        function getApproveList() {
-            adminAPI.getApproveList().then(function(data) {
-                vm.approveList = angular.copy(data);
+        $scope.$on('$stateChangeStart', function() {
+            vm.disconnect();
+        })
+
+        function connect() {
+            var socket = new SockJS('/api/approve_list');
+            stompClient = Stomp.over(socket);
+            stompClient.connect({}, function(frame) {
+                console.log('Connected: ' + frame);
+                stompClient.subscribe('/api/pipe/approve_list', function(greeting){
+                    console.log(greeting);
+                });
+
+                vm.send();
             });
         }
+
+        function disconnect() {
+            if (stompClient != null) {
+                stompClient.disconnect();
+            }
+            console.log("Disconnected");
+        }
+
+        function send() {
+            stompClient.send("/api/approve_list", {}, JSON.stringify({ 'page': 0, 'page_size': 10 }));
+        }
+
+        //function getApproveList() {
+        //    adminAPI.getApproveList().then(function(data) {
+        //        vm.approveList = angular.copy(data);
+        //    });
+        //}
+
+        //function disconnect() {
+        //    console.log('dicsonnecting');
+        //    $stomp.disconnect(function () {
+        //
+        //    });
+        //}
+
+        //function getApproveList() {
+        //    $stomp.connect('/ws/approve_list')
+        //        .then(function (frame) {
+        //            console.log('connect success');
+        //            $stomp.subscribe('/ws/pipe/approve_list', function (approveList) {
+        //                console.log(approveList);
+        //                vm.approveList.concat(approveList);
+        //            });
+        //
+        //            $stomp.send('/ws/approve_list', {
+        //                page: 0,
+        //                page_size: 10
+        //            }).then(function() {
+        //                console.log('send success');
+        //            });
+        //        });
+        //}
 
         function makeText(type, tableName) {
             switch(type) {
