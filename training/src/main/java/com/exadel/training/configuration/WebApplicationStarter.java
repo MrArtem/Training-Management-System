a693f381027d36c83c6c1b275871b979293f249a
Artem Yudovin <ayudovin@exadel.com>
2015-11-04 11:56:29 +0300

    Fix files

--- training/src/main/java/com/exadel/training/configuration/WebApplicationStarter.java
+++ training/src/main/java/com/exadel/training/configuration/WebApplicationStarter.java
@@ -1,17 +1,23 @@
 package com.exadel.training.configuration;
 
 import com.exadel.training.security.ApplicationSecurity;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.hibernate.SessionFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.util.SocketUtils;
 
 import javax.persistence.EntityManagerFactory;
+import java.io.IOException;
 
 @Configuration
 @EnableAutoConfiguration
--- training/src/main/java/com/exadel/training/service/FileStorageService.java
+++ training/src/main/java/com/exadel/training/service/FileStorageService.java
@@ -5,12 +5,13 @@ import com.exadel.training.dao.domain.FileStorage;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by ayudovin on 31.10.2015.
  */
 public interface FileStorageService {
-    void addFile(FileUpload fileUpload) throws IOException;
+    void addFile(Map<String, String> files, long idTraining)  throws IOException;
     void deleteFile(long idFileStorage);
 
     List<FileStorage> getAllFileByTraining(long idTraining);
