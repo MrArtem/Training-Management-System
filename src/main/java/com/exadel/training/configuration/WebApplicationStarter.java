8ab95fd7a4443ad4b8f89a37527dfae01a9984cd
Artem Yudovin <ayudovin@exadel.com>
2015-11-10 20:25:33 +0300

    Modify size file of upload

--- src/main/java/com/exadel/training/configuration/WebApplicationStarter.java
+++ src/main/java/com/exadel/training/configuration/WebApplicationStarter.java
@@ -39,7 +39,7 @@ public class WebApplicationStarter extends SpringBootServletInitializer {
         CommonsMultipartResolver resolver=new CommonsMultipartResolver();
         resolver.setDefaultEncoding("utf-8");
         resolver.setMaxInMemorySize(1048576);
-        resolver.setMaxUploadSize(26000000);
+        resolver.setMaxUploadSize(Integer.MAX_VALUE);
         return resolver;
     }
 }
