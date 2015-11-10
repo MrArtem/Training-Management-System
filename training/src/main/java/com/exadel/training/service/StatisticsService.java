9914008a4b48f702d13c0260edbf0b6b79e6feb2
AlexZapolski <lelik_lesha@hotmail.com>
2015-11-10 08:43:09 +0200

    change structure

--- training/src/main/java/com/exadel/training/service/StatisticsService.java
+++
@@ -1,8 +0,0 @@
-package com.exadel.training.service;
-
-import com.exadel.training.controller.model.StatisticsModel;
-import com.itextpdf.text.DocumentException;
-
-public interface StatisticsService {
-    String getStatistics(StatisticsModel statisticsModel) throws DocumentException;
-}
--- training/src/main/java/com/exadel/training/validate/annotation/LegalID.java
+++
@@ -1,7 +0,0 @@
-package com.exadel.training.validate.annotation;
-
-/**
- * Created by ayudovin on 24.10.2015.
- */
-public @interface LegalID {
-}
