b80a4fce62e65c480e7fb21e583074c4bc4dd24b
AAneitchik <aaneitchik@gmail.com>
2015-11-12 06:41:35 +0300

    managecourse page almost done

--- frontend/src/app/page.managecourse/managecourse.dates/dates.controller.js
+++ frontend/src/app/page.managecourse/managecourse.dates/dates.controller.js
@@ -1,22 +1,33 @@
 (function () {
-    'use strict';
+	'use strict';
 
-    angular
-        .module('tmsApp')
-        .controller('DatesController', DatesController);
+	angular
+		.module('tmsApp')
+		.controller('DatesController', DatesController);
 
-    /** @ngInject */
-    function DatesController($scope, $state, $stateParams, userAPI) {
-        var vm = this;
-        vm.changeTab = changeTab;
+	/** @ngInject */
+	function DatesController($scope, $state, $stateParams, userAPI) {
+		var vm = this;
+		vm.addDate = addDate;
+		vm.changeTab = changeTab;
 
-        function changeTab() {
-            if($scope.courseInfo.isRepeating) {
-                $state.transitionTo('managecourse.dates.repeat');
-            }
-            else {
-                $state.transitionTo('managecourse.dates.manual');
-            }
-        }
-    }
+		vm.days = ['M', 'T', 'W', 'Th', 'F', 'Sa', 'S'];
+		vm.onDays = [false, false, false, false, false, false, false];
+		vm.dates = [];
+
+		function addDate() {
+			$scope.courseInfo.lessonList.push({
+				date: (new Date(vm.tempDate)).getTime()
+			});
+		}
+
+		function changeTab() {
+			if ($scope.courseInfo.isRepeating) {
+				$state.transitionTo('managecourse.dates.repeat');
+			}
+			else {
+				$state.transitionTo('managecourse.dates.manual');
+			}
+		}
+	}
 })();
